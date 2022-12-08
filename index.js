5c8763c0d2404215958763d0979aaf58410001bc
appaandbuki <104958634+appaandbuki@users.noreply.github.com>
2022-12-08 16:57:47 -0700

    connecting DB to API on Heroku

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-
+//mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
