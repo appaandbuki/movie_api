84bd7c42d948e33f8aefedd2a7bfe74a56b4eec9
appaandbuki <104958634+appaandbuki@users.noreply.github.com>
2022-12-12 12:56:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 // Get all users
-app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/users', (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
